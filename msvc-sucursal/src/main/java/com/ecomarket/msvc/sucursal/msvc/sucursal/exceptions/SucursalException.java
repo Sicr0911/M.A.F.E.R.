2129a7cd9c941247164b2298b654e818339bd261
bfrey <ba.frey@duocuc.cl>
2025-05-22 19:45:12 -0400

    avance

---
+++ msvc-sucursal/src/main/java/com/ecomarket/msvc/sucursal/msvc/sucursal/exceptions/SucursalException.java
@@ -0,0 +1,7 @@
+package com.ecomarket.msvc.sucursal.msvc.sucursal.exceptions;
+
+public class SucursalException extends RuntimeException {
+    public SucursalException(String message) {
+        super(message);
+    }
+}
