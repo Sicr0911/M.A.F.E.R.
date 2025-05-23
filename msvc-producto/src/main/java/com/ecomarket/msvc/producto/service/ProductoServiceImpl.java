03440bec0f1f85df4681dcb531ea7eaa0ee17c85
benja <be.bastiasa@duocuc.cl>
2025-05-22 21:06:32 -0400

    Avance producto

---
+++ msvc-producto/src/main/java/com/ecomarket/msvc/producto/service/ProductoServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ecomarket.msvc.producto.service;
+
+import com.ecomarket.msvc.producto.exceptions.ProductoException;
+import com.ecomarket.msvc.producto.models.entities.Producto;
+import com.ecomarket.msvc.producto.repositories.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoServiceImpl implements ProductoService{
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Override
+    public List<Producto> findAll() {
+        return this.findAll();
+    }
+
+    @Override
+    public Producto findById(Long id) {
+        return this.productoRepository.findById(id).orElseThrow(
+                () -> new ProductoException("El producto con el id:" + id + "no existe")
+        );
+    }
+
+    @Override
+    public Producto save(Producto producto) {
+        return null;
+    }
+
+}
