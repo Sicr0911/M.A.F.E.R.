51dfac7832ae7fc4e6bd377f45c85021429337fc
bfrey <ba.frey@duocuc.cl>
2025-05-22 20:44:13 -0400

    arreglos

--- msvc-cliente/src/main/java/com/ecomarket/msvc/cliente/msvc_cliente/services/ClienteServiceImpl.java
+++ msvc-cliente/src/main/java/com/ecomarket/msvc/cliente/msvc_cliente/services/ClienteServiceImpl.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class ClienteServiceImpl implements ClienteService {
 
-
     @Autowired
     private ClienteRepository clienteRepository;
 
---
+++ msvc-sucursal/src/main/java/com/ecomarket/msvc/sucursal/msvc/sucursal/service/SucursalServiceImpl.java
@@ -0,0 +1,31 @@
+package com.ecomarket.msvc.sucursal.msvc.sucursal.service;
+
+import com.ecomarket.msvc.sucursal.msvc.sucursal.exceptions.SucursalException;
+import com.ecomarket.msvc.sucursal.msvc.sucursal.model.Sucursal;
+import com.ecomarket.msvc.sucursal.msvc.sucursal.repositories.SucursalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class SucursalServiceImpl implements SucursalService {
+
+    @Autowired
+    private SucursalRepository sucursalRepository;
+
+    @Override
+    public List<Sucursal> findAll() {
+        return this.findAll();
+    }
+
+    @Override
+    public Sucursal findById(Long id) {
+        return this.sucursalRepository.findById(id).orElseThrow(
+                () -> new SucursalException("La sucursal con el id:" + id + "no existe")
+        );
+    }
+
+    @Override
+    public Sucursal save(Sucursal sucursal) {
+        return null;
+    }
+}
