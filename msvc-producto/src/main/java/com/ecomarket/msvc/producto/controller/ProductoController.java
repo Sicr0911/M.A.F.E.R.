d6bddfd2e538b29341c9fe3c0e65ff95d6218206
bfrey <ba.frey@duocuc.cl>
2025-05-27 09:50:16 -0400

    fin

--- msvc-producto/src/main/java/com/ecomarket/msvc/producto/controller/ProductoController.java
+++ msvc-producto/src/main/java/com/ecomarket/msvc/producto/controller/ProductoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/inventarios")
+@RequestMapping("/api/v1/productos")
 @Validated
 
 public class ProductoController {
