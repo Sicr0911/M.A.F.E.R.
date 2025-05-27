d92bee404677ffec69b250b47b2fa3c9a6244cae
bfrey <ba.frey@duocuc.cl>
2025-05-27 10:18:05 -0400

    fin

--- msvc-cliente/src/main/java/com/ecomarket/msvc/cliente/msvc_cliente/model/entities/Cliente.java
+++ msvc-cliente/src/main/java/com/ecomarket/msvc/cliente/msvc_cliente/model/entities/Cliente.java
@@ -2,7 +2,6 @@ package com.ecomarket.msvc.cliente.msvc_cliente.model.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
