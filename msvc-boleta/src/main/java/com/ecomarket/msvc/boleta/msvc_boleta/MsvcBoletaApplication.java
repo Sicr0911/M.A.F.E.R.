b43c8949d2d6656d84ba9caedc3dcbeab2842985
Sicr0911 <seb.catalanr@duocuc.cl>
2025-05-27 21:03:23 -0400

    correccion de Algo

--- msvc-boleta/src/main/java/com/ecomarket/msvc/boleta/msvc_boleta/MsvcBoletaApplication.java
+++ msvc-boleta/src/main/java/com/ecomarket/msvc/boleta/msvc_boleta/MsvcBoletaApplication.java
@@ -2,7 +2,9 @@ package com.ecomarket.msvc.boleta.msvc_boleta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsvcBoletaApplication {
 
