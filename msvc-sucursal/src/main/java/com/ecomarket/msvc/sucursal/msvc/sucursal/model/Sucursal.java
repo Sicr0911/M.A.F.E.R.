d9f8dba5adfb42ddd8dfb32dba2835a4454d220e
bfrey <ba.frey@duocuc.cl>
2025-06-17 23:13:34 -0400

    service test de sucursale hecho

--- msvc-sucursal/src/main/java/com/ecomarket/msvc/sucursal/msvc/sucursal/model/Sucursal.java
+++ msvc-sucursal/src/main/java/com/ecomarket/msvc/sucursal/msvc/sucursal/model/Sucursal.java
@@ -1,5 +1,6 @@
 package com.ecomarket.msvc.sucursal.msvc.sucursal.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
@@ -7,22 +8,27 @@ import lombok.*;
 @Getter @Setter @ToString
 @NoArgsConstructor @AllArgsConstructor
 @Entity
+@Schema(description = "Entidad de sucursal")
 @Table(name="sucursal")
 public class Sucursal {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "id_sucursal", nullable = false, unique = true)
+    @Schema(description = "primary key sucursal", example = "1")
     private Long idSucursal;
 
     @Column(nullable = false)
     @NotBlank(message = "El campo nombre no puede ser vacio")
+    @Schema(description = "nombre sucursal", example = "Vecino del bolsillo", required = true)
     private String nombre;
 
     @Column(nullable = false)
     @NotBlank(message = "El campo direccion no puede ser vacio")
+    @Schema(description = "direccion sucursal", example = "Viña del mar", required = true)
     private String direccion;
 
+    @Schema(description = "numero sucursal", example = "1234567898")
     private String telefono;
 
 }
---
+++ msvc-sucursal/src/test/java/com/ecomarket/msvc/sucursal/msvc/sucursal/service/SucursalServiceTest.java
@@ -0,0 +1,115 @@
+package com.ecomarket.msvc.sucursal.msvc.sucursal.service;
+
+import com.ecomarket.msvc.sucursal.msvc.sucursal.exceptions.SucursalException;
+import com.ecomarket.msvc.sucursal.msvc.sucursal.model.Sucursal;
+import com.ecomarket.msvc.sucursal.msvc.sucursal.repositories.SucursalRepository;
+import net.datafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SucursalServiceTest {
+
+    @Mock
+    private SucursalRepository sucursalRepository ;
+
+    @InjectMocks
+    private SucursalServiceImpl sucursalService;
+
+    private Sucursal sucursalPrueba;
+
+    private List<Sucursal> sucursales = new ArrayList<>();
+
+    @BeforeEach
+    public void setUp() {
+        Faker faker = new Faker(Locale.of("es", "CL"));
+
+        for  (int i = 0; i < 100; i++) {
+
+            Sucursal sucursal = new Sucursal();
+            sucursal.setIdSucursal((long) i);
+            sucursal.setNombre(faker.name().fullName());
+            sucursal.setDireccion(faker.address().fullAddress());
+
+            this.sucursales.add(sucursal);
+        }
+        this.sucursalPrueba = new Sucursal(
+                1L, "Vecino del ahorro", "Viña del mar", "123456789"
+        );
+    }
+
+    @Test
+    @DisplayName("Debe listar todos las sucursales")
+    public void shouldFindAllSucursales() {
+        this.sucursales.add(this.sucursalPrueba) ;
+
+        when(sucursalRepository.findAll()).thenReturn(this.sucursales);
+
+        List<Sucursal> resultado = sucursalService.findAll();
+
+        assertThat(resultado).hasSize(101);
+        assertThat(resultado).contains(this.sucursalPrueba);
+
+        verify(sucursalRepository, times(1)).findAll();
+
+    }
+
+    @Test
+    @DisplayName("Debe encontrar una sucursal por id")
+    public void shouldFindSucursalById() {
+        when(sucursalRepository.findById(1L)).thenReturn(Optional.of(this.sucursalPrueba));
+
+        Sucursal result = sucursalService.findById(1L);
+
+        assertThat(result).isNotNull();
+        assertThat(result).isEqualTo(this.sucursalPrueba);
+
+        verify(sucursalRepository,times(1)).findById(1L);
+
+    }
+
+    @Test
+    @DisplayName("Debe entregar una excepcion cuando medico id no exista")
+    public void shouldNotFindSucursalById(){
+        Long idInexistente = 999L;
+
+        when(sucursalRepository.findById(idInexistente)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> {
+            sucursalService.findById(idInexistente);
+
+        }).isInstanceOf(SucursalException.class)
+                .hasMessageContaining("El medico con id " + idInexistente
+                        + " no se encuentra en la base de datos");
+
+        verify(sucursalRepository, times(1)).findById(idInexistente);
+    }
+
+    @Test
+    @DisplayName("Debe guardar un nuevo medico")
+    public void shouldSaveMedico(){
+        when(sucursalRepository.save(any(Sucursal.class))).thenReturn(this.sucursalPrueba);
+
+        Sucursal result = sucursalService.save(this.sucursalPrueba);
+
+        assertThat(result).isNotNull();
+        assertThat(result).isEqualTo(this.sucursalPrueba);
+
+        verify(sucursalRepository, times(1)).save(any(Sucursal.class));
+    }
+
+}
