e598210bc389cde30e819f4ac7212f73c434d81d
bfrey <ba.frey@duocuc.cl>
2025-06-23 22:10:40 -0400

    agrego los controles V2

--- msvc-sucursal/src/main/java/com/ecomarket/msvc/sucursal/msvc/sucursal/assemblers/SucursalModelAssembler.java
+++ msvc-sucursal/src/main/java/com/ecomarket/msvc/sucursal/msvc/sucursal/assemblers/SucursalModelAssembler.java
@@ -1,4 +1,24 @@
 package com.ecomarket.msvc.sucursal.msvc.sucursal.assemblers;
 
-public class SucursalModelAssembler {
+import com.ecomarket.msvc.sucursal.msvc.sucursal.controller.SucursalControllerV2;
+import com.ecomarket.msvc.sucursal.msvc.sucursal.model.Sucursal;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class SucursalModelAssembler implements RepresentationModelAssembler<Sucursal, EntityModel<Sucursal>> {
+    @Override
+    public EntityModel<Sucursal> toModel(Sucursal entity) {
+        return EntityModel.of(
+                entity,
+                linkTo(methodOn(SucursalControllerV2.class).findById(entity.getIdSucursal())).withSelfRel(),
+                linkTo(methodOn(SucursalControllerV2.class).findAll()).withRel("sucursal"),
+                Link.of("http://localhost:8006/sucursal/"+entity.getIdSucursal()).withRel("sucursal")
+        );
+    }
 }
--- msvc-sucursal/src/main/java/com/ecomarket/msvc/sucursal/msvc/sucursal/controller/SucursalControllerV2.java
+++ msvc-sucursal/src/main/java/com/ecomarket/msvc/sucursal/msvc/sucursal/controller/SucursalControllerV2.java
@@ -1,4 +1,109 @@
 package com.ecomarket.msvc.sucursal.msvc.sucursal.controller;
 
+import com.ecomarket.msvc.sucursal.msvc.sucursal.assemblers.SucursalModelAssembler;
+import com.ecomarket.msvc.sucursal.msvc.sucursal.dtos.ErrorDTO;
+import com.ecomarket.msvc.sucursal.msvc.sucursal.model.Sucursal;
+import com.ecomarket.msvc.sucursal.msvc.sucursal.service.SucursalService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("/api/v2/sucursales")
+@Validated
+@Tag(name = "Sucursales HATEOAS", description = "Esta sección contiene los CRUD de sucursal")
 public class SucursalControllerV2 {
+
+    @Autowired
+    private SucursalService sucursalService;
+
+    @Autowired
+    private SucursalModelAssembler  sucursalModelAssembler;
+
+    @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(
+            summary = "Devuelve todos los medicos",
+            description = "Este metodo debe retornar un List de Medico, en caso "+
+                    "de que no encuentre nada retorna una List vacia"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Se retornaron todos los medicos ok",
+                    content = @Content(
+                            mediaType = MediaTypes.HAL_JSON_VALUE,
+                            schema = @Schema(implementation = Sucursal.class)
+                    )
+            )
+    })
+
+    public ResponseEntity<CollectionModel<EntityModel<Sucursal>>> findAll() {
+        List<EntityModel<Sucursal>> entityModels = this.sucursalService.findAll()
+                .stream()
+                .map(sucursalModelAssembler::toModel)
+                .toList();
+        CollectionModel<EntityModel<Sucursal>> collectionModel = CollectionModel.of(
+                entityModels,
+                linkTo(methodOn(SucursalControllerV2.class).findAll()).withSelfRel()
+        );
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(collectionModel);
+    }
+
+    @GetMapping(value = "/{id}",  produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(
+            summary = "Devuelve un medico con respecto a su id",
+            description = "Este metodo debe retornar un Medico cuando es consultado "+
+                    "mediante su id"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Se retorna el medico encontrado",
+                    content = @Content(
+                            mediaType = MediaTypes.HAL_JSON_VALUE,
+                            schema = @Schema(implementation = Sucursal.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Error - Medico con ID no existe",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDTO.class)
+                    )
+            )
+    })
+            @Parameters(value = {
+                    @Parameter(name = "id", description = "Este es el id unico de una sucursal", required = true)
+            })
+    public ResponseEntity<EntityModel<Sucursal>> findById(@PathVariable Long id) {
+        EntityModel<Sucursal> entityModel = this.sucursalModelAssembler.toModel(
+                sucursalService.findById(id)
+        );
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(entityModel);
+    }
+
 }
